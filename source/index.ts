50f2567df77d164dbcb75f23771c5873145b13d6
skarab42 <skarab@bluewin.ch>
2024-01-09 20:53:09 +0100

    feat: result api

--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,45 @@
-export const api = { life: 42 };
+const successSymbol = Symbol('result:success');
+const failureSymbol = Symbol('result:failure');
+
+type SuccessSymbol = typeof successSymbol;
+type FailureSymbol = typeof failureSymbol;
+
+export type Success<SuccessValue> = { readonly [K in SuccessSymbol]: SuccessValue } & {
+  readonly [K in FailureSymbol]?: never;
+};
+
+export type Failure<FailureValue> = { readonly [K in FailureSymbol]: FailureValue } & {
+  readonly [K in SuccessSymbol]?: never;
+};
+
+export type Result<SuccessValue, FailureValue> = Success<SuccessValue> | Failure<FailureValue>;
+
+export function success<SuccessValue>(value: SuccessValue): Success<SuccessValue> {
+  return { [successSymbol]: value };
+}
+
+export function failure<FailureValue>(value: FailureValue): Failure<FailureValue> {
+  return { [failureSymbol]: value };
+}
+
+export function isSuccess<SuccessValue>(result: Result<SuccessValue, unknown>): result is Success<SuccessValue> {
+  return result[successSymbol] !== undefined;
+}
+
+export function isFailure<FailureValue>(result: Result<unknown, FailureValue>): result is Failure<FailureValue> {
+  return result[failureSymbol] !== undefined;
+}
+
+export function unwrap<SuccessValue>(result: Success<SuccessValue>): SuccessValue;
+
+export function unwrap<FailureValue>(result: Failure<FailureValue>): FailureValue;
+
+export function unwrap<SuccessValue, FailureValue>(
+  result: Result<SuccessValue, FailureValue>,
+): FailureValue | SuccessValue;
+
+export function unwrap<SuccessValue, FailureType>(
+  result: Result<SuccessValue, FailureType>,
+): FailureType | SuccessValue {
+  return isSuccess(result) ? result[successSymbol] : result[failureSymbol];
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,37 @@
 import { expect, it } from 'vitest';
 
-import { api } from '../source/index.js';
+import { failure, isFailure, isSuccess, type Result, success, unwrap } from '../source/index.js';
 
-it('should answer the question of life', () => {
-  expect(api.life).toBe(42);
+function test(condition: boolean): Result<true, false> {
+  return condition ? success(true) : failure(false);
+}
+
+it('should unwrap success value', () => {
+  const result = test(true);
+  const expectTrue = isSuccess(result);
+  const expectFalse = isFailure(result);
+
+  expect(expectTrue).toStrictEqual(true);
+  expect(expectFalse).toStrictEqual(false);
+  expect(unwrap(result)).toStrictEqual(true);
+  expect(result).toMatchInlineSnapshot(`
+    {
+      Symbol(result:success): true,
+    }
+  `);
+});
+
+it('should unwrap failure value', () => {
+  const result = test(false);
+  const expectTrue = isFailure(result);
+  const expectFalse = isSuccess(result);
+
+  expect(expectTrue).toStrictEqual(true);
+  expect(expectFalse).toStrictEqual(false);
+  expect(unwrap(result)).toStrictEqual(false);
+  expect(result).toMatchInlineSnapshot(`
+    {
+      Symbol(result:failure): false,
+    }
+  `);
 });
