d715c49ef0788f4d66b43595c41abb70f8b6d078
skarab42 <skarab@bluewin.ch>
2024-01-12 16:50:43 +0100

    docs: add some documentation

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # @skarab/result
 
+`@skarab/result` is a TypeScript library providing a robust pattern for handling success and failure outcomes in a functional programming style. Utilizing advanced TypeScript features, it allows developers to create expressive, type-safe APIs that clearly distinguish between successful results and errors.
+
+## Features
+
+- **Type-Safe Success and Failure Handling:** Clearly differentiate between success and failure cases using TypeScript types.
+- **Functional Approach:** Embrace a functional programming style for handling results.
+- **Easy to Use:** Simple API with powerful type inference.
+
+## Installation
+
+To install the package, use pnpm, npm or yarn:
+
 ## Installation
 
 ```bash
@@ -8,11 +20,92 @@ pnpm add @skarab/result
 
 ## Usage
 
-```ts
-import { api } from '@skarab/result';
+Here's a basic example of how to use `@skarab/result`:
 
-console.log(api.life); // 42
+```ts
+import { success, failure, isSuccess, unwrap } from '@skarab/result';
+
+function performOperation(): Result<number, Error> {
+  // some operation
+  if (/* operation successful */) {
+    return success(42);
+  } else {
+    return failure(new Error('Operation failed'));
+  }
+}
+
+const result = performOperation();
+
+if (isSuccess(result)) {
+  console.log('Success with value:', unwrap(result));
+} else {
+  console.log('Failure with error:', unwrap(result));
+}
 ```
+
+## API Documentation
+
+### Types
+
+#### `Success<SuccessValue>`
+
+- A type representing a successful result.
+- Contains a value of type `SuccessValue`.
+- Indexed by a unique success symbol, ensuring type safety and avoiding property clashes.
+
+#### `Failure<FailureValue>`
+
+- A type representing a failure result.
+- Contains a value of type `FailureValue`.
+- Indexed by a unique failure symbol, providing clear differentiation from the `Success` type.
+
+#### `Result<SuccessValue, FailureValue>`
+
+- A union type representing either a success or a failure.
+- Encapsulates `Success<SuccessValue>` and `Failure<FailureValue>`.
+
+### Functions
+
+#### `success<SuccessValue>(value: SuccessValue): Success<SuccessValue>`
+
+- Creates a success result containing the provided value.
+- Useful for wrapping successful outcomes in a type-safe manner.
+- **Parameters:**
+  - `value`: The value to be associated with the success.
+
+#### `failure<FailureValue>(value: FailureValue): Failure<FailureValue>`
+
+- Creates a failure result containing the provided value.
+- Ideal for expressing error states or unsuccessful outcomes.
+- **Parameters:**
+  - `value`: The value to be associated with the failure.
+
+#### `isSuccess<SuccessValue>(result: Result<SuccessValue, unknown>): result is Success<SuccessValue>`
+
+- A type guard function that checks if a given result is a success.
+- Returns `true` if the result is a success, otherwise `false`.
+- Helps in narrowing down the type of the result to `Success<SuccessValue>` when true.
+
+#### `isFailure<FailureValue>(result: Result<unknown, FailureValue>): result is Failure<FailureValue>`
+
+- A type guard function that checks if a given result is a failure.
+- Returns `true` if the result is a failure, otherwise `false`.
+- Assists in type narrowing to `Failure<FailureValue>` when the result is a failure.
+
+#### `unwrap<SuccessValue, FailureValue>(result: Result<SuccessValue, FailureValue>): FailureValue | SuccessValue`
+
+- Extracts the value from a success or failure result.
+- Simplifies the process of accessing the underlying value of a result.
+- **Parameters:**
+  - `result`: The `Result` instance from which the value should be unwrapped.
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request for any features or fixes.
+
+## License
+
+This project is licensed under the MIT License.
 
 ---
 
--- source/index.ts
+++ source/index.ts
@@ -1,43 +1,169 @@
+// Symbols for uniquely identifying success and failure cases.
 const successSymbol = Symbol('result:success');
 const failureSymbol = Symbol('result:failure');
 
+// Type aliases for the success and failure symbols.
 type SuccessSymbol = typeof successSymbol;
 type FailureSymbol = typeof failureSymbol;
 
-export type Success<SuccessValue> = { readonly [K in SuccessSymbol]: SuccessValue } & {
+/**
+ * Represents a successful {@link Result} with a value of type `SuccessValue`.
+ *
+ * - The success value is indexed by the {@link SuccessSymbol}.
+ * - A {@link FailureSymbol} will never be present in this type.
+ *
+ * @template SuccessValue - The type of the value associated with a successful result.
+ */
+export type Success<SuccessValue> = {
+  readonly [K in SuccessSymbol]: SuccessValue;
+} & {
   readonly [K in FailureSymbol]?: never;
 };
 
-export type Failure<FailureValue> = { readonly [K in FailureSymbol]: FailureValue } & {
+/**
+ * Represents a failure {@link Result} with a value of type `FailureValue`.
+ *
+ * - The failure value is indexed by the {@link FailureSymbol}.
+ * - A {@link SuccessSymbol} will never be present in this type.
+ *
+ * @template FailureValue - The type of the value associated with a failure result.
+ */
+export type Failure<FailureValue> = {
+  readonly [K in FailureSymbol]: FailureValue;
+} & {
   readonly [K in SuccessSymbol]?: never;
 };
 
+/**
+ * Represents a result that can either be a {@link success} or a {@link failure}.
+ *
+ * @template SuccessValue - The type of the value in case of success.
+ * @template FailureValue - The type of the value in case of failure.
+ *
+ * @example
+ * function life(age: number): Result<number, Error> {
+ *   return age === 42 ? success(42) : failure(new Error("c'est la vie!"));
+ * }
+ */
 export type Result<SuccessValue, FailureValue> = Success<SuccessValue> | Failure<FailureValue>;
 
+/**
+ * Creates a success {@link Result} with the given value.
+ *
+ * @template SuccessValue - The type of the value to be returned in case of success.
+ *
+ * @param value - The value to be associated with the success.
+ * @returns A success result containing the value.
+ *
+ * @example
+ * // Example usage:
+ * const mySuccess = success(100);
+ * console.log(mySuccess); // Outputs: { [Symbol(result:success)]: 100 }
+ */
 export function success<SuccessValue>(value: SuccessValue): Success<SuccessValue> {
   return { [successSymbol]: value };
 }
 
+/**
+ * Creates a failure {@link Result} with the given value.
+ *
+ * @template FailureValue - The type of the value to be returned in case of failure.
+ *
+ * @param value - The value to be associated with the failure.
+ * @returns A failure result containing the value.
+ *
+ * @example
+ * // Example usage:
+ * const myFailure = failure(new Error('Something went wrong'));
+ * console.log(myFailure); // Outputs: { [Symbol(result:failure)]: Error: Something went wrong }
+ */
 export function failure<FailureValue>(value: FailureValue): Failure<FailureValue> {
   return { [failureSymbol]: value };
 }
 
+/**
+ * Type guard to determine if the {@link Result} is a success.
+ *
+ * @template SuccessValue - The type of the success value to be checked.
+ *
+ * @param result - The result to check.
+ * @returns `true` if the result is a success, `false` otherwise.
+ *
+ * @example
+ * // Example usage:
+ * const myResult = success(42);
+ *
+ * if (isSuccess(myResult)) {
+ *   // TypeScript knows myResult is Success<number> here
+ * }
+ */
 export function isSuccess<SuccessValue>(result: Result<SuccessValue, unknown>): result is Success<SuccessValue> {
   return result[successSymbol] !== undefined;
 }
 
+/**
+ * Type guard to determine if the {@link Result} is a failure.
+ *
+ * @template FailureValue - The type of the failure value to be checked.
+ *
+ * @param result - The result to check.
+ * @returns `true` if the result is a failure, `false` otherwise.
+ *
+ * @example
+ * // Example usage:
+ * const myResult = failure(new Error('Failed'));
+ *
+ * if (isFailure(myResult)) {
+ *   // TypeScript knows myResult is Failure<Error> here
+ * }
+ */
 export function isFailure<FailureValue>(result: Result<unknown, FailureValue>): result is Failure<FailureValue> {
   return result[failureSymbol] !== undefined;
 }
 
+/**
+ * Unwraps the value from a success or failure {@link Result}.
+ *
+ * - If the result is a success, returns the success value.
+ * - If the result is a failure, returns the failure value.
+ *
+ * @template SuccessValue - The type of the success value to be unwrapped.
+ * @template FailureValue - The type of the failure value to be unwrapped.
+ *
+ * @param result - The {@link Result} to unwrap.
+ * @returns The unwrapped value.
+ *
+ * @example
+ * // Basic usage:
+ * const successResult = success(10);
+ * const failureResult = failure(new Error('Failed'));
+ * const value1 = unwrap(successResult); // value1 is 10
+ * const value2 = unwrap(failureResult); // value2 is Error: Failed
+ *
+ * @example
+ * // Advenced usage:
+ * function life(age: number): Result<number, Error> {
+ *   return age === 42 ? success(42) : failure(new Error("c'est la vie!"));
+ * }
+ *
+ * const result = life(42);
+ *
+ * if (isSuccess(result)) {
+ *   const value = unwrap(result);
+ *   // result: Success<number>
+ *   // value: number
+ * } else {
+ *   const value = unwrap(result);
+ *   // result: Failure<Error>
+ *   // value: Error
+ * }
+ *
+ */
 export function unwrap<SuccessValue>(result: Success<SuccessValue>): SuccessValue;
-
 export function unwrap<FailureValue>(result: Failure<FailureValue>): FailureValue;
-
 export function unwrap<SuccessValue, FailureValue>(
   result: Result<SuccessValue, FailureValue>,
 ): FailureValue | SuccessValue;
-
 export function unwrap<SuccessValue, FailureType>(
   result: Result<SuccessValue, FailureType>,
 ): FailureType | SuccessValue {
